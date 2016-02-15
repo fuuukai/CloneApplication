972376a080882c1f819d6b3aa4651b8f7ee512c8
fuuukai <caxa8.fukai@gmail.com>
2016-02-15 20:24:43 +0900

    変数m追加

--- app/src/main/java/com/lifeistech/android/cloneapplication/MainActivity.java
+++ app/src/main/java/com/lifeistech/android/cloneapplication/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends AppCompatActivity {
     int i = 0;
     int n = 0;
 
+    int m = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
