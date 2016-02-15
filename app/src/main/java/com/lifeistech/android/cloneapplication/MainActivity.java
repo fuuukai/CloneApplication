26a25917edab6127f9dae936a760bf52dc26d1de
fuuukai <caxa8.fukai@gmail.com>
2016-02-15 20:00:28 +0900

    変数追加

--- app/src/main/java/com/lifeistech/android/cloneapplication/MainActivity.java
+++ app/src/main/java/com/lifeistech/android/cloneapplication/MainActivity.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     int i = 0;
+    int n = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
 }
